9d39781573557dbf6fee34db7f139fdcfe8592cd
Miselia92 <ashe9200@gmail.com>
2026-01-21 18:30:50 +0800

    feat: Implement and integrate an `OptimizedImage` component across pages to enhance image loading and performance.

--- src/content/pages/home.md
+++ src/content/pages/home.md
@@ -2,8 +2,10 @@
 title: Home
 brandName: Tina
 heroImages:
+  - /images/20230112_071933.jpg
   - /images/3.jpg
   - /images/example-1.jpg
   - /images/example-3.jpg
+  - /images/藝廊-22-1.jpg
 ---
 
--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'local', queries,  });
+export const client = createClient({ cacheDir: '/home/miselia/web/tina/__generated__/.cache/1768990664981', url: 'http://localhost:4001/graphql', token: '733e4c7fa07535fc5ef66ec0974cc8a8d1a43e73', queries,  });
 export default client;
   
